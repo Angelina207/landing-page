791e0123491d3a4e0d87dac174ea31975cd26f1e
Angelina <75496111+Angelina207@users.noreply.github.com>
2022-01-28 17:04:59 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,94 +1,44 @@
-//  scroll
-
-$(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-});
-
-
-// slider to features section
-
-$(document).ready(function() {
-    let position = 0;
-    const slideToShow = 1;
-    const slideToScroll = 1;
-    const container = $('.slide-container');
-    const track = $('.slide-track');
-    const item = $('.slide-item');
-    const btnNext = $('.btn-next');
-    const btnPrev = $('.btn-prev');
-    const itemsCount = item.length;
-    const itemWidth = container.width() / slideToShow;
-    const movePosition = slideToScroll * itemWidth;
-
-    item.each(function (index, item) {
-        $(item).css({
-            minWidth: itemWidth,
-        });
-    });
-
-    btnPrev.click(function(){
-        position  += movePosition;
-
-        setPosition();
-        checkBtns();
-    });
-
-    btnNext.click(function(){
-        position  -= movePosition;
-
-        setPosition();
-        checkBtns();
-    });
-
-    const setPosition = function() {
-        track.css({
-            transform: `translateX(${position}px)`
-        });
-    };
-
-    const checkBtns = function() {
-
-        btnPrev.prop('disabled', position === 0);
-        btnNext.prop('disabled', position <= - (itemsCount - slideToShow) * itemWidth
-        
-        );
-    };
-
-    checkBtns();
-});
-
-
 // burger menu
 
 function burgerMenu(selector) {
-    let menu = $(selector);
-    let burgerBtn = menu.find('.burger-menu__btn');
-    let links = menu.find('.head__link');
-    let overlay = menu.find('.burger-menu__overlay');
+    let menu = document.querySelector(selector);
+    let burgerBtn = menu.querySelector('.burger-menu__btn');
+    let links = menu.querySelector('.head__link');
+    let overlay = menu.querySelector('.burger-menu__overlay');
 
-    burgerBtn.on('click', (e) => {
+    burgerBtn.addEventListener('click', (e) => {
         e.preventDefault();
         toggleMenu();
     }); 
 
-    links.on('click', () => toggleMenu());
-    overlay.on('click', () => toggleMenu());
+    links.addEventListener('click', () => toggleMenu());
+    overlay.addEventListener('click', () => toggleMenu());
 
     function toggleMenu() {
-
-        menu.toggleClass('burger-menu__active');
-
-        if (menu.hasClass('burger-menu__active')) {
-            $('body').css('overflow', 'hidden');
+        menu.classList.toggle("burger-menu__active");
+        if (menu.classList.contains("burger-menu__active")) {
+            document.body.style.overflow = "hidden";
         } else {
-        $('body').css('overflow', 'visible');
+            document.body.style.overflow = "visible";
+        }
     }
 }
-}
-
 burgerMenu('.burger__menu');
+
+
+//  scroll
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
